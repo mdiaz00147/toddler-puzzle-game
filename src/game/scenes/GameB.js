f6d56dd5ed2ffd1fdb9d24754d7d19eff0c786f6
MarioD <mario@mario.com>
2024-10-18 12:42:10 -0400

    sounds added and main menu

--- src/game/scenes/GameB.js
+++ src/game/scenes/GameB.js
@@ -1,120 +1,131 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
 export class GameB extends Phaser.Scene {
   constructor() {
-    super({ key: 'GameB' })
+    super({ key: 'GameB' });
+
+    this.platforms = null;
+    this.player = null;
+    this.cursors = null;
+    this.stars = null;
+    this.bombs = null;
+    this.scoreText = null;
+    this.gameOver = false;
+    this.score = 0;
   }
 
   preload() {
-    this.sWidth = this.cameras.main.width
-    this.sHeight = this.cameras.main.height
-
-    this.add
-      .image(this.sWidth / 2, this.sHeight / 2, 'sky')
-      .setDisplaySize(this.sWidth, this.sHeight)
+    this.load.image('sky', 'assets/elements/sky.png');
+    this.load.image('ground', 'assets/elements/platform.png');
+    this.load.image('star', 'assets/elements/star.png');
+    this.load.image('bomb', 'assets/elements/bomb.png');
+    this.load.spritesheet('dude', 'assets/elements/dude.png', { frameWidth: 32, frameHeight: 48 });
   }
 
   create() {
-    // Platforms
-    this.platforms = this.physics.add.staticGroup()
-    this.platforms
-      .create(this.sWidth / 2, this.sHeight - 32, 'ground')
-      .setScale(2)
-      .refreshBody()
-    this.platforms.create(this.sWidth * 0.75, this.sHeight * 0.7, 'ground')
-    this.platforms.create(this.sWidth * 0.1, this.sHeight * 0.5, 'ground')
-    this.platforms.create(this.sWidth * 0.9, this.sHeight * 0.4, 'ground')
+    this.add.image(400, 300, 'sky');
 
-    // Player
-    this.player = this.physics.add
-      .sprite(100, 450, 'dude')
-      .setBounce(0.4)
-      .setCollideWorldBounds(true)
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
       frameRate: 10,
       repeat: -1
-    })
-    this.anims.create({ key: 'turn', frames: [{ key: 'dude', frame: 4 }], frameRate: 20 })
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20
+    });
+
     this.anims.create({
       key: 'right',
       frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
       frameRate: 10,
       repeat: -1
-    })
+    });
 
-    // Input
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Stars
     this.stars = this.physics.add.group({
       key: 'star',
       repeat: 11,
       setXY: { x: 12, y: 0, stepX: 70 }
-    })
-    this.stars.children.iterate((child) => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)))
+    });
 
-    // Bombs
-    this.bombs = this.physics.add.group()
+    this.stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
 
-    // Score
-    this.score = 0
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' })
+    this.bombs = this.physics.add.group();
 
-    // Colliders
-    this.physics.add.collider(this.player, this.platforms)
-    this.physics.add.collider(this.stars, this.platforms)
-    this.physics.add.collider(this.bombs, this.platforms)
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this)
-    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this)
+    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.stars, this.platforms);
+    this.physics.add.collider(this.bombs, this.platforms);
+    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
   }
 
   update() {
     if (this.gameOver) {
-      return
+      return;
     }
 
     if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160)
-      this.player.anims.play('left', true)
+      this.player.setVelocityX(-160);
+      this.player.anims.play('left', true);
     } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160)
-      this.player.anims.play('right', true)
+      this.player.setVelocityX(160);
+      this.player.anims.play('right', true);
     } else {
-      this.player.setVelocityX(0)
-      this.player.anims.play('turn')
+      this.player.setVelocityX(0);
+      this.player.anims.play('turn');
     }
 
     if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330)
+      this.player.setVelocityY(-330);
     }
   }
 
   collectStar(player, star) {
-    star.disableBody(true, true)
-    this.score += 10
-    this.scoreText.setText('Score: ' + this.score)
+    star.disableBody(true, true);
 
-    if (this.stars.countActive(true) === 0) {
-      this.stars.children.iterate((child) => child.enableBody(true, child.x, 0, true, true))
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
 
-      const x = player.x < 400 ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400)
-      const bomb = this.bombs.create(x, 16, 'bomb')
-      bomb.setBounce(1)
-      bomb.setCollideWorldBounds(true)
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
-      bomb.allowGravity = false
-    }
+    // if (this.stars.countActive(true) === 0) {
+      this.stars.children.iterate(function (child) {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+
+      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+      var bomb = this.bombs.create(x, 16, 'bomb');
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+      bomb.allowGravity = false;
+    // }
   }
 
   hitBomb(player, bomb) {
-    this.physics.pause()
-    player.setTint(0xff0000)
-    player.anims.play('turn')
-    this.gameOver = true
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    this.gameOver = true;
   }
 }
 
-export default GameB
-
+export default GameB;
--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -15,80 +15,34 @@ export class MainMenu extends Scene {
   }
 
   create() {
-    this.changeScene('GameA') // dev only
+    // Create title text
+    this.add.text(this.sWidth / 2, this.sHeight / 4, 'Main Menu', {
+      fontSize: `${this.fontSize * 1.5}px`,
+      fill: '#ffffff'
+    }).setOrigin(0.5)
 
-    this.add
-      .image(this.sWidth / 2, this.sHeight / 2, 'background')
-      .setDisplaySize(this.sWidth, this.sHeight)
+    // Create GameA button
+    const gameAButton = this.add.text(this.sWidth / 2, this.sHeight / 2, 'Play GameA', {
+      fontSize: `${this.fontSize}px`,
+      fill: '#ffffff'
+    }).setOrigin(0.5).setInteractive()
 
-    this.add.image(this.sWidth / 2, this.sHeight / 2, 'logan-a')
+    gameAButton.on('pointerdown', () => {
+      this.changeScene('GameA')
+    })
 
-    this.add
-      .text(this.sWidth / 2, this.sHeight / 2 + 400, 'Empezar Game A!', {
-        fontFamily: 'Arial Black',
-        fontSize: this.fontSize,
-        color: '#ffffff',
-        stroke: '#000000',
-        strokeThickness: 8,
-        align: 'center'
-      })
-      .setDepth(100)
-      .setOrigin(0.5)
-      .setInteractive() // Make the text interactive
-      .on('pointerdown', () => {
-        this.changeScene('GameA')
-      })
+    // Create GameB button
+    const gameBButton = this.add.text(this.sWidth / 2, this.sHeight / 2 + this.fontSize * 2, 'Play GameB', {
+      fontSize: `${this.fontSize}px`,
+      fill: '#ffffff'
+    }).setOrigin(0.5).setInteractive()
 
-    // this.add
-    //   .text(this.sWidth / 2, this.sHeight / 2 + 500, 'Empezar Game B!', {
-    //     fontFamily: 'Arial Black',
-    //     fontSize: this.fontSize,
-    //     color: '#ffffff',
-    //     stroke: '#000000',
-    //     strokeThickness: 8,
-    //     align: 'center'
-    //   })
-    //   .setDepth(100)
-    //   .setOrigin(0.5)
-    //   .setInteractive() // Make the text interactive
-    //   .on('pointerdown', () => {
-    //     this.changeScene('GameB')
-    //   })
-
-    EventBus.emit('current-scene-ready', this)
+    gameBButton.on('pointerdown', () => {
+      this.changeScene('GameB')
+    })
   }
 
   changeScene(sceneName) {
-    if (this.logoTween) {
-      this.logoTween.stop()
-      this.logoTween = null
-    }
-
     this.scene.start(sceneName)
   }
-
-  moveLogo(vueCallback) {
-    if (this.logoTween) {
-      if (this.logoTween.isPlaying()) {
-        this.logoTween.pause()
-      } else {
-        this.logoTween.play()
-      }
-    } else {
-      this.logoTween = this.tweens.add({
-        targets: this.logo,
-        x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-        y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-        yoyo: true,
-        repeat: -1,
-        onUpdate: () => {
-          vueCallback({
-            x: Math.floor(this.logo.x),
-            y: Math.floor(this.logo.y)
-          })
-        }
-      })
-    }
-  }
 }
-
